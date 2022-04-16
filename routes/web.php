89cbd4969b54e2c80f2c4575c371a2d3686f4e01
artem <artem@koorochka.com>
2022-04-16 17:07:28 +0300

    Controller with router for Catalog products

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route,
+    App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect("/", "/catalog");
+Route::resource("/catalog", ProductController::class);
